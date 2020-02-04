b71a4c322f3ed2107d3d6d21fa8f50418bb6519e
Michelle Wong <michellekwong2@gmail.com>
2020-02-04 12:09:54 -0500

    time is updated to current time & formatted for scheduling meal

--- src/components/MyCalendar.js
+++ src/components/MyCalendar.js
@@ -3,61 +3,70 @@ import moment from 'moment'
 import { Calendar, momentLocalizer } from "react-big-calendar";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 
-moment.locale("en-GB");
-//https://momentjs.com/timezone/
-// var dec = moment("2014-12-01T12:00:00Z");
-// dec.tz('America/New_York').format('ha z'); 
-
-const localizer = momentLocalizer(moment);
-var today = new Date();
-var date = (today.getMonth()+1)+'/'+today.getDate() + '/' + today.getFullYear();
-
-const MyCalendar = () => (
-  <div>
-     <Calendar
-      localizer={localizer}
-      events={[
+class MyCalendar extends React.Component{
+    state = {
+      events: [
         {
           title: "Eat chicken parm now",
           allDay: false,
-          start: new Date(2020, 0, 31, 8, 15), // 8.15 AM on 2nd
-          end: new Date(2020, 0, 31, 14, 0) // 2.00 PM on 2nd
+          start: new Date(2020, 1, 4, 8, 15), // 8.15 AM on 2nd
+          end: new Date(2020, 1, 4, 14, 0) // 2.00 PM on 2nd
         },
-        {
-            title: "Go get that bacon egg n cheeeez to start the day",
-            allDay: false,
-            start: new Date(2020, 0, 29, 8, 15), // 8.15 AM on 2nd
-            end: new Date(2020, 0, 31, 14, 0) // 2.00 PM on 2nd
-          }
-      ]}
-      step={30} //diff b/w hrs (i.e. 30 min interval)
-      timeslots={1} //amount of lines in each hr
-      view="week"
-      views={["week"]}
-      min={new Date(2020, 2, 1, 8, 0)} // Calendar starts at 8.00 AM
-      max={new Date(2020, 2, 1, 20, 0)} // Calendar goes to max time of 8.00 PM
-      date={date} //calendar week rendered is determined by the current date
-    
-    //event styling
-    eventPropGetter={() => {
-        let newStyle = {
-            backgroundColor: "maroon",
-            color: 'white',
-            borderRadius: "15px", //rounds border
-            borderColor:"white",
-            borderWidth: "2px", //how thick the border is
-            fontFamily: "cursive",
-            fontSize: "20px",
-            textAlign: "center"
-        };
-        return {
-            className: "",
-            style: newStyle
-        };
-        }
+         {
+             title: "Go get that bacon egg n cheeeez to start the day",
+             allDay: false,
+             start: new Date(2020, 1, 7, 8, 15), // 8.15 AM on 2nd
+             end: new Date(2020, 1, 7, 10, 30) // 10.30 PM on 2nd
+           }
+      ]
     }
-    />
-    />
-  </div>
-)
+
+  render(){
+    moment.locale("en-GB");
+    //https://momentjs.com/timezone/
+    // var dec = moment("2014-12-01T12:00:00Z");
+    // dec.tz('America/New_York').format('ha z'); 
+    
+    const localizer = momentLocalizer(moment);
+    var today = new Date();
+    var date = (today.getMonth()+1)+'/'+today.getDate() + '/' + today.getFullYear();
+    console.log(today)
+    return(
+      <div>
+      <Calendar
+       localizer={localizer}
+       events={this.state.events}
+       step={30} //diff b/w hrs (i.e. 30 min interval)
+       timeslots={1} //amount of lines in each hr
+       defaultView="week"
+       views={["week"]}
+       min={new Date(2020, 2, 1, 8, 0)} // Calendar starts at 8.00 AM
+       max={new Date(2020, 2, 1, 20, 0)} // Calendar goes to max time of 8.00 PM
+       onNavigate={date => {
+         this.setState({ selectedDate: date });
+       }}
+       Date={date} //calendar week rendered is determined by the current date
+     
+     //event styling
+     eventPropGetter={() => {
+         let newStyle = {
+             backgroundColor: "maroon",
+             color: 'white',
+             borderRadius: "15px", //rounds border
+             borderColor:"white",
+             borderWidth: "2px", //how thick the border is
+             fontFamily: "cursive",
+             fontSize: "20px",
+             textAlign: "center"
+         };
+          return {
+             className: "",
+             style: newStyle
+          };
+         }}
+      />
+   </div>
+    )
+  }
+}
 export default MyCalendar;
--- src/components/ScheduleMeal.js
+++ src/components/ScheduleMeal.js
@@ -2,29 +2,47 @@ import React from 'react';
 import TextField from '@material-ui/core/TextField';
 import MyCalendar from './MyCalendar';
 
+const today = new Date();
+const date = (today.getMonth()+1)+'/'+ today.getDate() + '/' + today.getFullYear();
+
+const month = today.getMonth() <= 10 ?  '0' + (today.getMonth()+1) : (today.getMonth()+1)
+const day = today.getDate() <= 10 ?  '0' + today.getDate() : today.getDate()
+const timez = today.getMinutes() <= 10 ?  '0' + today.getMinutes() : today.getMinutes()
+const time = today.getHours() + ":" + timez ;
+const currDate = date+' '+time;
+
 class ScheduleMeal extends React.Component {
   
-  render() {
+  state ={
+    defaultValue: today.getFullYear() + '-' + month + '-' + day + 'T' + time
+  }
 
-    var today = new Date();
-    var date = (today.getMonth()+1)+'/'+today.getDate() + '/' + today.getFullYear();
-    var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-    var currDate = date+' '+time;
-    
-    
+  changeDate = (e) => {
+    console.log(e.target)
+  }
+
+  render() {
+    console.log(this.state.defaultValue)
     return (
+      
       <div> 
         <h3>Today's date: {currDate}</h3>
         <form noValidate>
+            <h3>Add recipe to calendar</h3>
+            <input type="text" placeholder="Recipe Name" name="recipe"/>
+            <br />
             <TextField
             id="datetime-local"
             label="Schedule meal"
             type="datetime-local"
-            defaultValue="2020-01-31T10:30"  
+            defaultValue={this.state.defaultValue}  
+            onChange={e => this.changeDate(e)}
             InputLabelProps={{
                 shrink: true,
             }}
             />
+            <br />
+            <button type="submit" onClick={() => {console.log()}}>Submit</button>
         </form>
         <MyCalendar />
       </div>
